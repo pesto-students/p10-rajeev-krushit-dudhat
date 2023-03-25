d687a19de4c6594a0f5f6753293a21737b953d36
krushit dudhat <kruhsitdudhat2001@gmail.com>
2023-03-25 20:09:06 +0530

    fix: create stack function

--- Week-3/ex4.js
+++ Week-3/ex4.js
@@ -1,20 +1,32 @@
 function createStack() {
-  let _items = [];
+  const items = [];
   return {
     push(item) {
-      _items.push(item);
-      console.log(_items);
+      items.push(item);
+      console.log(items);
     },
     pop(item) {
-      console.log(_items);
-      return _items.pop(item);
+      console.log(this);
+      return items.pop(item);
     }
   };
 }
+
+// function createStack() {
+//   const items = [];
+//   const push = (item) => {
+//     this.items.push(item);
+//     console.log(this);
+//   };
+//   const pop = (item) => {
+//     console.log(this);
+//     return this.items.pop(item);
+//   };
+//   return { push, pop };
+// }
 
 const stack = createStack();
 stack.push(10);
 stack.push(5);
 console.log(stack.pop());
-console.log(stack._items);
-
+console.log(stack.items);
